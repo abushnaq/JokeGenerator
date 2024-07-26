ebe16d336b2fc1bf4bcf51e5eb60c8e1308d62a5
Ahmad Bushnaq <abushnaq@gmail.com>
2024-07-26 11:25:51 -0400

    Initial Commit

---
+++ JokeGenerator/JokeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeGeneratorApp.swift
+//  JokeGenerator
+//
+//  Created by Ahmad Remote on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct JokeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
